cdadfd59d73f7f5bf6daf191951359fc0afe9022
unknown <mobilecoecog@gmail.com>
2020-07-08 13:24:09 +0530

    C:\Users\Sharanya\Documents\docker-project Updated code to run in headless chrome and added chrome options

--- src/test/java/com/cognizant/dockerProject/DockerChrome.java
+++ src/test/java/com/cognizant/dockerProject/DockerChrome.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -16,21 +17,34 @@ public class DockerChrome {
 	public static void main(String[] args) throws MalformedURLException {
 //		WebDriver driver = new FirefoxDriver();
 		
-		DesiredCapabilities dc =  DesiredCapabilities.chrome();
-		
-		URL url = new URL("http://localhost:4444/wd/hub");
-		
-		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
-		driver.get("https://google.com");
-		System.out.println("the title is: " +driver.getTitle());
-		
-		// standalone file
-//		WebDriver driver;
-//		 WebDriverManager.chromedriver().setup();
-//
-//         driver = new ChromeDriver();
+//		DesiredCapabilities dc =  DesiredCapabilities.chrome();
+//		
+//		URL url = new URL("http://0.0.0.0:4444/wd/hub");
+//		
+//		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
 //		driver.get("https://google.com");
 //		System.out.println("the title is: " +driver.getTitle());
+		
+		// standalone file
+		
+ 
+		 WebDriverManager.chromedriver().setup();
+
+		    //Create Chrome Options
+		    ChromeOptions option = new ChromeOptions();
+		    option.addArguments("--no-sandbox");
+		    option.addArguments("--headless");
+		    option.addArguments("--disable-dev-shm-usage");
+		    DesiredCapabilities chrome = DesiredCapabilities.chrome();
+		    chrome.setJavascriptEnabled(true);
+		    option.setCapability(ChromeOptions.CAPABILITY, option);
+
+		    //Create driver object for Chrome
+		    WebDriver driver = new ChromeDriver(option);
+
+         
+		driver.get("https://google.com");
+		System.out.println("the title is: " +driver.getTitle());
 		
 		driver.quit();
 	}
